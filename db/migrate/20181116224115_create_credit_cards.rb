420e8e318ae5f0ec611e118c285379213ddece99
Andrei Loghin <andrei.loghin.17@aberdeen.ac.uk>
2018-11-17 00:10:24 +0000

    migration fix

--- db/migrate/20181116224115_create_credit_cards.rb
+++ db/migrate/20181116224115_create_credit_cards.rb
@@ -5,7 +5,7 @@ class CreateCreditCards < ActiveRecord::Migration[5.2]
       t.string :exp_date
       t.string :name_on_card
       t.string :organisation
-      t.references :customer, foreign_key: true
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
