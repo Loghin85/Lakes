acdf7694b70fcfb1d78c915fdc722f3c37517323
Andrei Loghin <andrei.loghin.17@aberdeen.ac.uk>
2018-11-19 16:17:34 +0000

    activation fix2

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,7 +1,7 @@
 class AccountActivationsController < ApplicationController
 
   def edit
-    user = User.find_by(Email: params[:Email])
+    user = User.find_by(Email: params[:email])
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
       user.activate
       log_in user
