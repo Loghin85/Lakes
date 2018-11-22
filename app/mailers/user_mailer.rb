7894bcfeac682f1d4d9662d13e02c9fbd11f408c
Andrei Loghin <andrei.loghin.17@aberdeen.ac.uk>
2018-11-22 17:02:38 +0000

    passwoed reset fix4

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -15,7 +15,7 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.password_reset.subject
   #
-  def password_reset
+  def password_reset(user)
     @user = user
     mail to: user.Email, subject: "Password reset"
   end
