af64e9b1a548f028c723ea0a8de07c459434b9fa
Andrei Loghin <andrei.loghin.17@aberdeen.ac.uk>
2018-12-01 01:51:56 +0000

    Flash fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
       if user && user.authenticate(params[:session][:password])
 		if user.activated?
 			log_in user
+			flash[:info] = 'Logged in successfully'
 			params[:session][:remember_me] == '1' ? remember(user) : forget(user)
 			redirect_to root_url
 		  else
